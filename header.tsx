ec11f114639e25fd861e17a374b4627916edd3b6
Oniza Fazal <160499036+Oniza94@users.noreply.github.com>
2024-12-10 00:12:21 +0500

    Add files via upload

--- header.tsx
+++ header.tsx
@@ -31,6 +31,8 @@ const UpperHeader = () => {
 
 export default UpperHeader;
 
+import Link from "next/link";
+
 export const LowerHeader = () => {
   return(
     <header className="flex border-b py-3 px-4 sm:px-10 bg-white font-[sans-serif] min-h-[70px] tracking-wide relative z-50">
@@ -39,33 +41,41 @@ export const LowerHeader = () => {
   
           <div className="items-centre justify-center flex px-3 ml-15 gap-3">
             <ul className="items-centre justify-center flex px-3 gap-3 ml-15">
-               <li className="max-lg:border-b max-lg:py-3 px-3">
-          <a
-            href="javascript:void(0)"
-            className="lg:hover:text-[#007bff] text-black block font-semibold text-[15px]"
-          >
+              
+            <a
+             href="/shop" 
+               className="mx-lg:border-b max-lg:py-3"
+           >
+            <li className="lgpx-3lg:hover:text-[#007bff] text-black block font-semibold text-[15px]">
+            
             Shop
-          </a>
+          
         </li>
+        </a>
+              
+        <a 
+        href="/OnSale"
+         className="max-lg:border-b max-lg:py-3"> 
+        <li className="px-3lg:hover:text-[#007bff] text-black block font-semibold text-[15px]"
+            >
+                On Sale
+      
+        </li>
+        </a>
+
+      
+
         <li className="max-lg:border-b max-lg:py-3 px-3">
           <a
-            href="javascript:void(0)"
-            className="lg:hover:text-[#007bff] text-black block font-semibold text-[15px]"
-          >
-            On Sale
-          </a>
-        </li>
-        <li className="max-lg:border-b max-lg:py-3 px-3">
-          <a
-            href="javascript:void(0)"
+            href="/Newarrivals"
             className="lg:hover:text-[#007bff] text-gray-800 block font-semibold text-[15px]"
           >
             New Arrivals
           </a>
         </li>
         <li className="max-lg:border-b max-lg:py-3 px-3">
-          <a
-            href="javascript:void(0)"
+          <a 
+            href="/Brands"
             className="lg:hover:text-[#007bff] text-black block font-semibold text-[15px]"
           >
             Brands
@@ -89,8 +99,11 @@ export const LowerHeader = () => {
         >
           <path d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z"></path>
         </svg>
+       
       </div>
+
     </div>
+   
   </div>
 </header>
 
--- page.tsx
+++ page.tsx
@@ -1,52 +1,30 @@
-const Customers = () => {
-  return(
-    <div className="p-4 font-sans">
-  <div className="max-w-6xl mx-auto bg-white rounded-lg shadow-md">
-    <div className="p-6 border-b">
-      <h2 className="text-3xl font-extrabold text-black text-4xl">
-        OUR HAPPY CUSTOMERS 
-      </h2>
-    </div>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
-      <div className="bg-indigo-50 p-6 rounded-lg border border-indigo-200">
-        <h3 className="text-lg font-semibold text-black font-bold">
-          Sarah M.
-        </h3>
-        <p className="text-gray-600 text-sm mt-4">
-          "I'm blown away by quality and style of the
-          clothes I received from Shop.co. From casual
-          wear to elegant dresses, every piece I've bought
-          has exceeded my expectations"
-        </p>
-      </div>
-      <div className="bg-indigo-50 p-6 rounded-lg border border-indigo-200">
-        <h3 className="text-lg font-semibold text-black font-bold">
-          Alek K.
-        </h3>
-        <p className="text-gray-600 text-sm mt-4">
-          "Findings clothes that alignwith my personal
-          style used to be challenge until I discovered 
-          Shop.co. The range of options they offer is truly
-          remarkable, catering to veriety of taste and 
-          occasions."
-        </p>
-      </div>
-      <div className="bg-indigo-50 p-6 rounded-lg border border-indigo-200">
-        <h3 className="text-lg font-semibold text-black font-bold mb-2">
-          Jamed L.
-        </h3>
-        <p className="text-gray-600 text-sm mt-4">
-         "As someone who's always on the lookout for 
-         unique fashion pieces, I'm thrilled to have
-         stumbled upon Shop.co. The selecction of 
-         clothes is not diverse but also on-point
-         with the latest trends"
-        </p>
-      </div>
-    </div>
-  </div>
-</div>
+import Image from "next/image";
+import UpperHeader from "./components/header";
+import { LowerHeader } from "./components/header";
+import NavBar from "./components/Navbar/page";
+import NewArrival from "./components/Navbar/NewArrival/page";
+import TopSelling from "./components/Navbar/TopSelling/page";
+import { ArrivalDown } from "./components/Navbar/TopSelling/page";
+import Customers from "./Customers/page";
+import Footer from "./components/footer";
+import { Children } from "react";
+import Shop from "./components/shop/page";
+export default function Home() {
+  return (
+    <>
+    
+    <UpperHeader />
+    <LowerHeader />
+    <NavBar />
+    <NewArrival />
+    <TopSelling />
+    <ArrivalDown />
+    <Customers />
+    <Footer />
 
-  )
-};
-export default Customers
+  
+    
+    </>
+
+     );
+}
